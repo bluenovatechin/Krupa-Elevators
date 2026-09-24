19322d0916bb226705f538191b106889c0e9b33c
Vyom Patel <vyom1912@gmail.com>
2026-09-24 10:37:32 +0530

    build done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const base = process.env.VITE_BASE_URL ?? '/KrupaElevator/';
+const base = process.env.VITE_BASE_URL ?? "/KrupaElevator/";
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  // base,
+  base: "/Krupa-Elevators/",
   server: {
     port: 3000,
-    open: false
-  }
+    open: false,
+  },
 });
